74d904249d1c593d80659e6e363f7a0a4c96668f
Mike Nedelko <mnedelko@gmail.com>
2018-02-04 11:39:40 -0800

    new commit

---
+++ models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = mongoose.Schema({
+
+	name 			: String,
+	email			: String, 
+	hashed_password	: String,
+	created_at		: String,
+	temp_password	: String,
+	temp_password_time: String
+});
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://mnedelko:b#2582@ds111568.mlab.com:11568/mascapone_dbv1');
+
+module.exports = mongoose.model('user', userSchema);
